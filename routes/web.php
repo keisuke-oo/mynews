dba7b5753b1c1e7301d073824bcd3725e6987b58
keisuke <ookumanote@gmail.com>
2019-12-12 02:22:09 +0000

    [課題提出]PHP/Laravel 12 ユーザー認証を実装する

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-  Route::get('news/create', 'Admin\NewsController@add');
+  Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 #課題３
@@ -24,9 +24,12 @@ Route::get('admin/XXX/', 'Admin\AAAController@bbb');
 
 #課題４
 Route::group(['prefix' => 'admin'], function() {
-  Route::get('profile/create', 'Admin\ProfileController@add');
+  Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
 });
 
 Route::group(['prefix' => 'admin'], function() {
-  Route::get('profile/edit', 'Admin\ProfileController@edit');
+  Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
