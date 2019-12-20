2fc0430ec1adf28954d3e1ee59da5eb375403fd8
keisuke <ookumanote@gmail.com>
2019-12-20 00:43:57 +0000

    PHP/Laravel 16[課題１]

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,17 +5,14 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //以下、追加分
     public function add()
     {
       return view('admin.profile.create');
-    }
-    
-    public function create()
-    {
-      return redirect('admin/profile/create');
     }
     
     public function edit()
@@ -27,4 +24,20 @@ class ProfileController extends Controller
     {
       return redirect('admin/profile/edit');
     }
+    
+#以下、16「課題１」にて追記。元あったpublic function create()は削除
+    public function create(Request $request)
+      {
+
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
+
+      $profile = new Profile;
+      $form = $request->all();
+      // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+
+      return redirect('admin/profile/create');
+      }
 }
