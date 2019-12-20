4dd8f14e3c604e92a8e7f09648c36096148998e0
keisuke <ookumanote@gmail.com>
2019-12-20 07:41:12 +0000

    課題取組中

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -6,8 +6,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 use App\Profile;
-use Carbon\Carbon;
 use App\History;
+use Carbon\Carbon;
+
 
 class ProfileController extends Controller
 {
@@ -17,13 +18,40 @@ class ProfileController extends Controller
       return view('admin.profile.create');
     }
     
-    public function edit()
+    public function edit(Request $request)
     {
+      // Profile Modelからデータを取得する
+      $profiles = Profile::find($request->id);
+      if (empty($profile)) {
+        abort(404);    
+      }
       return view('admin.profile.edit');
     }
     
-    public function update()
+    public function update(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        if ($request->remove == 'true') {
+            $profile_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path = $request->file('image')->store('public/image');
+            $profile_form['image_path'] = basename($path);
+        } else {
+            $profile_form['image_path'] = $news->image_path;
+        }
+
+        unset($profile_form['_token']);
+        unset($profile_form['image']);
+        unset($profile_form['remove']);
+        $profile->fill($profile_form)->save();    
+          //編集履歴を追加
+      $history = new History;
+      $history->profile_id = $profile -> id;
+      $history->edited_at = Carbon::now();
+      $history->save();  
+      
       return redirect('admin/profile/edit');
     }
     
@@ -39,20 +67,19 @@ class ProfileController extends Controller
       // データベースに保存する
       $profile->fill($form);
       $profile->save();
-      
-      //編集履歴を追加
-      $history = new History;
-      $history->profile_id = $profile -> id;
-      $history->edited_at = Carbon::now();
-      $history->save();
 
       return redirect('admin/profile/create');
       }
       
-    public function index(Request $request)
+    public function index()
      {
-
-      return view('admin.profile.index');
+      $cond_title = $request->cond_title;
+      if ($cond_title != '') {
+          $posts = Profile::where('title', $cond_title)->get();
+      } else {
+          $posts = Profile::all();
+      }       
+      return view('admin.profile.index', ['posts' => $posts, 'cond_title' => $cond_title]);
       }      
       
       
