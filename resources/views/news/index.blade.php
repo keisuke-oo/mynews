d9938dc15af6a90c007e617a79e41260568ac3bd
keisuke <ookumanote@gmail.com>
2019-12-24 06:12:13 +0000

    second commit

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <hr color="#c0c0c0">
         @if (!is_null($headline))
+        <p>課題用メッセージです</p>
             <div class="row">
                 <div class="headline col-md-10 mx-auto">
                     <div class="row">
